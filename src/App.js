0ea5244efe80b3fb139d9eaa3b08c60f2f4b4143
Navodita Sharma <navoditasharma16@gmail.com>
2020-12-20 18:58:10 +0530

    Add IPFS functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ipfs from './ipfs';
+import { Form, Button, FormGroup, FormControl, ControlLabel} from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    ipfsHash: null,
+    buffer: '',
+    filename: ''
+  };
+
+  captureFile =(event) => {
+    event.stopPropagation()
+    event.preventDefault()
+    const file = event.target.files[0]
+    this.setState({filename:file.name})
+    let reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => this.convertToBuffer(reader)    
+  };
+  convertToBuffer = async(reader) => {
+        const buffer = await Buffer.from(reader.result);
+        this.setState({buffer});
+    };
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    await ipfs.add(this.state.buffer, (err, ipfsHash) => {
+        console.log(err,ipfsHash, this.state.filename); 
+        this.setState({ ipfsHash:ipfsHash[0].hash });
+      })
+  };
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <h1> Insert Page</h1>
+          </header>
+          
+          <hr />
+            <h3> Choose file to send to IPFS </h3>
+            <Form onSubmit={this.onSubmit}>
+            <input 
+              type = "file"
+              onChange = {this.captureFile}
+            />
+             <Button 
+             bsStyle="primary" 
+             type="submit"> 
+             Send it 
+             </Button>
+            </Form>
+            <hr/>
+         </div> 
+      );
+  }
+
 }
 
 export default App;

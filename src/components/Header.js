d7422be7323e7271e4263670d9de1b54f33be905
Vimarsh Sathia <vimarsh.sathia@gmail.com>
2020-12-25 20:26:48 +0530

    Fixed refresh bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
 import React from "react";
 import {Navbar, Nav} from 'react-bootstrap';
+import {IndexLinkContainer} from 'react-router-bootstrap';
 
 export class Header extends React.Component {
     render() {
         return (
         <Navbar bg="light" expand="lg">
             {/* Printing account address currently using dapp */}
-            <Navbar.Brand href="/">{this.props.user.substring(0,5) + ".."}</Navbar.Brand>
+            <IndexLinkContainer to="/">
+            <Navbar.Brand>
+                {this.props.user.substring(0,5) + ".."}
+            </Navbar.Brand>
+            </IndexLinkContainer>
+
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/send">Send Data</Nav.Link>
+                    <IndexLinkContainer to="/">
+                        <Nav.Link>Home</Nav.Link>
+                    </IndexLinkContainer>
+
+                    <IndexLinkContainer to="/send">
+                        <Nav.Link>Send Data</Nav.Link>
+                    </IndexLinkContainer>
                 </Nav>
             </Navbar.Collapse>
             

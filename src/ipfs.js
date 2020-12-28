ec82d35109d40ee48891f4c3d9bdb384898a4e6b
Vimarsh Sathia <vimarsh.sathia@gmail.com>
2020-12-29 03:11:44 +0530

    Hide key

--- src/ipfs.js
+++ src/ipfs.js
@@ -1,10 +1,3 @@
-//using the infura.io node, otherwise ipfs requires you to run a //daemon on your own computer/server.
-// const IPFS = require('ipfs-api');
 const ipfsClient = require('ipfs-http-client')
-// const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'http' });
-const ipfs = ipfsClient({host: '34.93.89.211', port: '80', protocol: 'http'})
-//run with local daemon
-// const ipfsApi = require(‘ipfs-api’);
-// const ipfs = new ipfsApi(‘localhost’, ‘5001’, {protocol:‘http’});
-
+const ipfs = ipfsClient({host: process.env.NGINX_SERVER_KEY, port: '80', protocol: 'http'})
 export default ipfs;
